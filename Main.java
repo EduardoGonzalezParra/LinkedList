b13143f4d1c85b7629175ce0d793305c6443f3be
Eduardo Gonzalez Parra <eduardogp2203@gmail.com>
2021-02-21 21:06:23 -0600

    LinkedList

---
+++ Main.java
@@ -0,0 +1,12 @@
+package uaslp.ingenieria.labs;
+
+import uaslp.ingenieria.labs.list.LinkedList;
+import uaslp.ingenieria.labs.list.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        uaslp.ingenieria.labs.list.Node node = new Node();
+    }
+}
