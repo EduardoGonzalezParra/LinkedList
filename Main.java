fa96d4372cc348be0eea8f48b8f3499597331601
EduardoGonzalezParra <eduardogp2203@gmail.com>
2021-03-17 22:47:33 -0600

    Enum position

--- Main.java
+++ Main.java
@@ -1,12 +1,77 @@
 package uaslp.ingenieria.labs;
 
+import uaslp.ingenieria.labs.list.Iterator;
 import uaslp.ingenieria.labs.list.LinkedList;
-import uaslp.ingenieria.labs.list.Node;
+import uaslp.ingenieria.labs.list.Position;
+import uaslp.ingenieria.labs.list.ReverseIterator;
 
 public class Main {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        uaslp.ingenieria.labs.list.Node node = new Node();
+
+        list.add(1);
+        list.add(4);
+        list.add(3);
+        list.add(7);
+
+        System.out.println("El tamaño de la lista es: " + list.getSize());
+
+        Iterator it = list.getIterator();
+        ReverseIterator it2 = list.getReverseIterator();
+
+        System.out.println("-------------------------------------");
+        System.out.println("Recorrido inverso");
+
+        while(it2.hasNext()){
+            int element = it2.next();
+
+            System.out.println("Dato: " + element);
+        }
+
+        System.out.println("-------------------------------------");
+        System.out.println("Recorrido normal");
+
+        while(it.hasNext()){
+            Iterator backupIt = new Iterator(it);
+            int element = it.next();
+
+            if(element == 1){
+                list.insert(10, Position.BEFORE, backupIt);
+            }
+
+            if(element == 7){
+                list.insert(15, Position.AFTER, backupIt);
+            }
+
+            System.out.println("Dato: " + element);
+        }
+
+
+        System.out.println("-------------------------------------");
+        System.out.println("Lista con datos agregados");
+
+        it = list.getIterator();
+
+        while(it.hasNext()) {
+            int element = it.next();
+
+            System.out.println("Dato: " + element);
+        }
+
+        System.out.println("-------------------------------------");
+        System.out.println("Recorrido inverso con datos agregados");
+
+        it2 = list.getReverseIterator();
+
+        while(it2.hasNext()){
+            int element = it2.next();
+
+            System.out.println("Dato: " + element);
+        }
     }
 }
+
+//Clases: main, linkedlist, system
+//Objetos: list, out
+//Métodos: add, getsize, main, println
